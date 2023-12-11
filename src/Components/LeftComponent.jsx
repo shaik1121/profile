6e22d87d366d91eee140d678b8f8a134b4bd6f78
Shaik Muzakkir Hussain <sk.muzakkirhussain@gmail.com>
2023-12-11 23:44:24 +0530

    NEW: Branch create-feature

---
+++ src/Components/LeftComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Components/LeftComponent.scss";
+
+function LeftComponent() {
+  return (
+    <>
+      <div className="leftComponent">Hello</div>
+    </>
+  );
+}
+
+export default LeftComponent;
